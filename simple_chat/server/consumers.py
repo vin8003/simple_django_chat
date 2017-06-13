57e33814a5d043ad9a9072e524814caf3ac0d900
vin8003 <vin8003@gmail.com>
2017-06-13 08:57:05 +0530

    chat room handlers added like: join, leave..

--- simple_chat/server/consumers.py
+++ simple_chat/server/consumers.py
@@ -1,40 +1,73 @@
-from channels.auth import channel_session_user_from_http, channel_session_user
 import json
 from channels import Channel
+from channels.auth import channel_session_user_from_http, channel_session_user
+
+from .settings import MSG_TYPE_LEAVE, MSG_TYPE_ENTER, NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS
 from .models import Room
+from .utils import get_room_or_error, catch_client_error
+from .exceptions import ClientError
 
 
-# This decorator copies the user from the HTTP session (only available in
-# websocket.connect or http.request messages) to the channel session (available
-# in all consumers with the same reply_channel, so all three here)
 @channel_session_user_from_http
 def ws_connect(message):
-    # Initialise their session
     message.channel_session['rooms'] = []
+
+
+def ws_receive(message):
+    payload = json.loads(message['text'])
+    payload['reply_channel'] = message.content['reply_channel']
+    Channel("chat.receive").send(payload)
 
 
 @channel_session_user
 def ws_disconnect(message):
-    # Unsubscribe from any connected rooms
     for room_id in message.channel_session.get("rooms", set()):
         try:
             room = Room.objects.get(pk=room_id)
-            # Removes us from the room's send group. If this doesn't get run,
-            # we'll get removed once our first reply message expires.
             room.websocket_group.discard(message.reply_channel)
         except Room.DoesNotExist:
             pass
 
 
-# Unpacks the JSON in the received WebSocket frame and puts it onto a channel
-# of its own with a few attributes extra so we can route it
-# This doesn't need @channel_session_user as the next consumer will have that,
-# and we preserve message.reply_channel (which that's based on)
-def ws_receive(message):
-    # All WebSocket frames have either a text or binary payload; we decode the
-    # text part here assuming it's JSON.
-    # You could easily build up a basic framework that did this encoding/decoding
-    # for you as well as handling common errors.
-    payload = json.loads(message['text'])
-    payload['reply_channel'] = message.content['reply_channel']
-    Channel("chat.receive").send(payload)
+@channel_session_user
+@catch_client_error
+def chat_join(message):
+    room = get_room_or_error(message["room"], message.user)
+
+    if NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS:
+        room.send_message(None, message.user, MSG_TYPE_ENTER)
+
+    room.websocket_group.add(message.reply_channel)
+    message.channel_session['rooms'] = list(set(message.channel_session['rooms']).union([room.id]))
+    message.reply_channel.send({
+        "text": json.dumps({
+            "join": str(room.id),
+            "title": room.title,
+        }),
+    })
+
+
+@channel_session_user
+@catch_client_error
+def chat_leave(message):
+    room = get_room_or_error(message["room"], message.user)
+
+    if NOTIFY_USERS_ON_ENTER_OR_LEAVE_ROOMS:
+        room.send_message(None, message.user, MSG_TYPE_LEAVE)
+
+    room.websocket_group.discard(message.reply_channel)
+    message.channel_session['rooms'] = list(set(message.channel_session['rooms']).difference([room.id]))
+    message.reply_channel.send({
+        "text": json.dumps({
+            "leave": str(room.id),
+        }),
+    })
+
+
+@channel_session_user
+@catch_client_error
+def chat_send(message):
+    if int(message['room']) not in message.channel_session['rooms']:
+        raise ClientError("ROOM_ACCESS_DENIED")
+    room = get_room_or_error(message["room"], message.user)
+    room.send_message(message["message"], message.user)

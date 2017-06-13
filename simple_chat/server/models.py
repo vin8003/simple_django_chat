0be991833d12e50f23c813a32bbfe8e1b4759152
vin8003 <vin8003@gmail.com>
2017-06-13 08:33:47 +0530

    added chat rooms and some error handling

--- simple_chat/server/models.py
+++ simple_chat/server/models.py
@@ -1,3 +1,27 @@
+from channels import Group
 from django.db import models
+from django.utils.six import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Room(models.Model):
+    """
+    A room for people to chat in.
+    """
+
+    # Room title
+    title = models.CharField(max_length=255)
+
+    # If only "staff" users are allowed (is_staff on django's User)
+    staff_only = models.BooleanField(default=False)
+
+    def str(self):
+        return self.title
+
+    @property
+    def websocket_group(self):
+        """
+        Returns the Channels Group that sockets should subscribe to to get sent
+        messages as they are generated.
+        """
+        return Group("room-%s" % self.id)
--- simple_chat/server/views.py
+++ simple_chat/server/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Room
 
 
 @login_required
@@ -8,5 +9,10 @@ def index(request):
     Root page view. This is essentially a single-page app, if you ignore the
     login and admin parts.
     """
+    # Get a list of rooms, ordered alphabetically
+    rooms = Room.objects.order_by("title")
+
     # Render that in the index template
-    return render(request, "index.html")
+    return render(request, "index.html", {
+        "rooms": rooms,
+    })

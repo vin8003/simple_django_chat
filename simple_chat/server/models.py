d2f8cf8c865e7942f25e8c9e15ab9aa053009bf2
vin8003 <vin8003@gmail.com>
2017-06-13 08:50:36 +0530

    added msg send method in models

--- simple_chat/server/models.py
+++ simple_chat/server/models.py
@@ -1,6 +1,9 @@
-from channels import Group
+import json
 from django.db import models
 from django.utils.six import python_2_unicode_compatible
+from channels import Group
+
+from simple_chat.settings import MSG_TYPE_MESSAGE
 
 
 @python_2_unicode_compatible
@@ -25,3 +28,14 @@ class Room(models.Model):
         messages as they are generated.
         """
         return Group("room-%s" % self.id)
+
+    def send_message(self, message, user, msg_type=MSG_TYPE_MESSAGE):
+        """
+        Called to send a message to the room on behalf of a user.
+        """
+        final_msg = {'room': str(self.id), 'message': message, 'username': user.username, 'msg_type': msg_type}
+
+        # Send out the message to everyone in the room
+        self.websocket_group.send(
+            {"text": json.dumps(final_msg)}
+        )

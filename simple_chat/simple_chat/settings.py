c9e7cebf0b628f128178cd9ddbf7b0b9c2fa724f
vin8003 <vin8003@gmail.com>
2017-06-13 08:15:30 +0530

    static files loaded up

--- simple_chat/simple_chat/settings.py
+++ simple_chat/simple_chat/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # redis channel
 
 redis_host = os.environ.get('REDIS_HOST', 'localhost')
